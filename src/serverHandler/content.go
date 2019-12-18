2736c09b87da0016244c345410c4edaa99a0ad3d
marjune <marjune@163.com>
2019-12-18 15:27:10 +0800

    fix(serverHandler): output Content-Type header by HEAD method for file content

--- src/serverHandler/content.go
+++ src/serverHandler/content.go
@@ -1,11 +1,42 @@
 package serverHandler
 
-import "net/http"
+import (
+	"mime"
+	"net/http"
+	"os"
+	"path"
+)
+
+func getContentType(item os.FileInfo, file *os.File) (string, error) {
+	ext := path.Ext(item.Name())
+	ctype := mime.TypeByExtension(ext)
+	if len(ctype) > 0 {
+		return ctype, nil
+	}
+
+	var buf [512]byte
+	n, err := file.Read(buf[:])
+	if err != nil {
+		return ctype, err
+	}
+
+	ctype = http.DetectContentType(buf[:n])
+	return ctype, nil
+}
 
 func (h *handler) content(w http.ResponseWriter, r *http.Request, data *responseData) {
+	item := data.Item
+	file := data.File
+
 	if needResponseBody(r.Method) {
-		item := data.Item
-		file := data.File
 		http.ServeContent(w, r, item.Name(), item.ModTime(), file)
+		return
+	}
+
+	ctype, err := getContentType(item, file)
+	if err == nil {
+		w.Header().Set("Content-Type", ctype)
+	} else {
+		w.WriteHeader(http.StatusInternalServerError)
 	}
 }
