e14dc90a61aed42ef5b55151380018ea0f8c8dce
marjune <marjune@163.com>
2020-03-16 11:05:46 +0800

    fix(test): make sure required commands exists

--- test/main.bash
+++ test/main.bash
@@ -2,6 +2,14 @@
 
 cd $(dirname $0)
 
+for cmd in realpath curl; do
+	type "$cmd" &> /dev/null
+	if [ $? -ne 0 ]; then
+		echo "command '$cmd' not found" >&2
+		exit 1
+	fi
+done
+
 export root=$(realpath .)
 export fs=$(realpath fs)
 export cert=$(realpath cert)
