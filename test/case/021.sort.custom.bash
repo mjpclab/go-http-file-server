86ea984d3c333511ba51c69edf13d06f033557bf
marjune <marjune@163.com>
2020-04-27 12:11:29 +0800

    test(sort): add test case for sort transitivity

---
+++ test/case/021.sort.custom.bash
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+cleanup() {
+	rm -f "$fs"/downloaded/*.tmp
+}
+
+source "$root"/lib.bash
+
+"$ghfs" -l 3003 -r "$fs"/vhost1 &
+sleep 0.05 # wait server ready
+cleanup
+
+curl_get_body 'http://127.0.0.1:3003/' | grep -q -F 'go/"'
+if [ $? -ne 0 ]; then
+	fail "unexpected url param for sort"
+fi
+
+cleanup
+kill %1
