176c4566aa9b685571622799e03601106acc7b8c
marjune <marjune@163.com>
2020-12-30 14:23:21 +0800

    fix(serverHandler): prevent mutation from parent directory
    When passing file path for Upload, Mkdir and Delete, treat parent
    directory path as illegal path.

--- src/serverHandler/delete.go
+++ src/serverHandler/delete.go
@@ -1,14 +1,24 @@
 package serverHandler
 
 import (
+	"errors"
 	"os"
 )
 
 func (h *handler) deleteItems(fsPrefix string, files []string, aliasSubItems []os.FileInfo) bool {
 	errs := []error{}
 
-	for _, filename := range files {
-		if containsItem(aliasSubItems, filename) {
+	for _, inputFilename := range files {
+		if len(inputFilename) == 0 {
+			continue
+		}
+
+		var filename string
+		var ok bool
+		if filename, ok = getCleanFilePath(inputFilename); !ok {
+			errs = append(errs, errors.New("delete: illegal item name "+inputFilename))
+			continue
+		} else if containsItem(aliasSubItems, filename) {
 			continue
 		}
 		err := os.RemoveAll(fsPrefix + "/" + filename)
