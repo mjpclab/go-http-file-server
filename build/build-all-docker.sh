c55bb44f55d1c6d6e00d41e15e148a779c95d81d
marjune <marjune@163.com>
2021-02-17 19:05:43 +0800

    feat(tpl): use `go:embed` to embed asserts
    This makes minimal required Go version up to 1.16. So also remove build
    scripts that work for previous version.

--- build/build-all-docker.sh
+++ build/build-all-docker.sh
@@ -35,25 +35,6 @@ builds=("${builds[@]}" 'netbsd 386' 'netbsd amd64' 'netbsd arm' 'netbsd arm64')
 builds=("${builds[@]}" 'plan9 386' 'plan9 amd64' 'plan9 arm')
 buildByDocker "$gover" "${builds[@]}"
 
-#gover=1.9
-#builds=('freebsd 386 -9.x' 'freebsd amd64 -9.x' 'freebsd arm -9.x')
-#buildByDocker "$gover" "${builds[@]}"
-
-gover=1.10
-builds=('windows 386 -xp-vista' 'windows amd64 -xp-vista')
-#builds=("${builds[@]}" 'darwin 386 -10.8-mountain_lion' 'darwin amd64 -10.8-mountain_lion')
-#builds=("${builds[@]}" 'openbsd 386 -6.0' 'openbsd amd64 -6.0' 'openbsd arm -6.0')
-buildByDocker "$gover" "${builds[@]}"
-
-#gover=1.12
-#builds=('darwin 386 -10.10-yosemite' 'darwin amd64 -10.10-yosemite')
-#builds=("${builds[@]}" 'freebsd 386 -10.x' 'freebsd amd64 -10.x' 'freebsd arm -9.x')
-#buildByDocker "$gover" "${builds[@]}"
-
-#gover=1.14
-#builds=('darwin 386 -10.11-el_capitan' 'darwin amd64 -10.11-el_capitan')
-#buildByDocker "$gover" "${builds[@]}"
-
 #gover=1.16
 #builds=('darwin amd64 -10.12-sierra')
 #buildByDocker "$gover" "${builds[@]}"
