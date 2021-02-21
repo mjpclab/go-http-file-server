09e6c524050b090826c889757407ca64b7db9ca7
marjune <marjune@163.com>
2021-02-21 10:50:27 +0800

    chore: disable build on latest Go version
    It is the main branch's duty to maintain build on latest Go version.

--- build/build-all-docker.sh
+++ build/build-all-docker.sh
@@ -27,16 +27,21 @@ buildByDocker() {
     "$@"
 }
 
-gover=latest
-builds=('linux 386' 'linux amd64' 'linux arm' 'linux arm64' 'windows 386' 'windows amd64' 'windows arm' 'darwin amd64' 'darwin arm64')
-builds=("${builds[@]}" 'freebsd 386' 'freebsd amd64' 'freebsd arm' 'freebsd arm64')
-builds=("${builds[@]}" 'openbsd 386' 'openbsd amd64' 'openbsd arm' 'openbsd arm64')
-builds=("${builds[@]}" 'netbsd 386' 'netbsd amd64' 'netbsd arm' 'netbsd arm64')
-builds=("${builds[@]}" 'plan9 386' 'plan9 amd64' 'plan9 arm')
-buildByDocker "$gover" "${builds[@]}"
+#gover=1.15
+#builds=('linux 386' 'linux amd64' 'linux arm' 'linux arm64' 'windows 386' 'windows amd64' 'windows arm' 'darwin amd64' 'darwin arm64')
+#builds=("${builds[@]}" 'freebsd 386' 'freebsd amd64' 'freebsd arm' 'freebsd arm64')
+#builds=("${builds[@]}" 'openbsd 386' 'openbsd amd64' 'openbsd arm' 'openbsd arm64')
+#builds=("${builds[@]}" 'netbsd 386' 'netbsd amd64' 'netbsd arm' 'netbsd arm64')
+#builds=("${builds[@]}" 'plan9 386' 'plan9 amd64' 'plan9 arm')
+#buildByDocker "$gover" "${builds[@]}"
 
-#gover=1.9
-#builds=('freebsd 386 -9.x' 'freebsd amd64 -9.x' 'freebsd arm -9.x')
+#gover=1.14
+#builds=('darwin 386 -10.11-el_capitan' 'darwin amd64 -10.11-el_capitan')
+#buildByDocker "$gover" "${builds[@]}"
+
+#gover=1.12
+#builds=('darwin 386 -10.10-yosemite' 'darwin amd64 -10.10-yosemite')
+#builds=("${builds[@]}" 'freebsd 386 -10.x' 'freebsd amd64 -10.x' 'freebsd arm -9.x')
 #buildByDocker "$gover" "${builds[@]}"
 
 gover=1.10
@@ -45,15 +50,6 @@ builds=('windows 386 -xp-vista' 'windows amd64 -xp-vista')
 #builds=("${builds[@]}" 'openbsd 386 -6.0' 'openbsd amd64 -6.0' 'openbsd arm -6.0')
 buildByDocker "$gover" "${builds[@]}"
 
-#gover=1.12
-#builds=('darwin 386 -10.10-yosemite' 'darwin amd64 -10.10-yosemite')
-#builds=("${builds[@]}" 'freebsd 386 -10.x' 'freebsd amd64 -10.x' 'freebsd arm -9.x')
-#buildByDocker "$gover" "${builds[@]}"
-
-#gover=1.14
-#builds=('darwin 386 -10.11-el_capitan' 'darwin amd64 -10.11-el_capitan')
-#buildByDocker "$gover" "${builds[@]}"
-
-#gover=1.16
-#builds=('darwin amd64 -10.12-sierra')
+#gover=1.9
+#builds=('freebsd 386 -9.x' 'freebsd amd64 -9.x' 'freebsd arm -9.x')
 #buildByDocker "$gover" "${builds[@]}"
