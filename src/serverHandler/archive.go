89e49216588c436d36728874f287f44d180221c2
marjune <marjune@163.com>
2021-03-28 22:22:27 +0800

    fix(serverHandler/archive): skip readdir from "/" if empty root

--- src/serverHandler/archive.go
+++ src/serverHandler/archive.go
@@ -14,6 +14,7 @@ type archiveCallback func(f *os.File, fInfo os.FileInfo, relPath string) error
 
 func (h *handler) visitFs(
 	initFsPath, rawRequestPath, relPath string,
+	statFs bool,
 	filterCallback filterCallback,
 	archiveCallback archiveCallback,
 ) {
@@ -26,46 +27,54 @@ func (h *handler) visitFs(
 		fsPath = initFsPath
 	}
 
+	if hasAlias && rawRequestPath != "/" {
+		statFs = true
+	}
+
 	var fInfo os.FileInfo
 	var childInfos []os.FileInfo
+	if statFs {
+		// wrap func to run defer ASAP
+		err := func() error {
+			f, err := os.Open(fsPath)
+			if f != nil {
+				defer f.Close()
+			}
+			h.errHandler.LogError(err)
 
-	err := func() error {
-		f, err := os.Open(fsPath)
-		if f != nil {
-			defer f.Close()
-		}
-		h.errHandler.LogError(err)
+			if err != nil {
+				if os.IsExist(err) {
+					return err
+				}
+				fInfo = newFakeFileInfo(path.Base(fsPath), true) // prefix path for alias
+			} else {
+				fInfo, err = f.Stat()
+				if h.errHandler.LogError(err) {
+					return err
+				}
+			}
 
+			if len(relPath) > 0 {
+				if err := archiveCallback(f, fInfo, relPath); err != nil {
+					return err
+				}
+			}
+
+			if f != nil && fInfo.IsDir() {
+				childInfos, err = f.Readdir(0)
+				if h.errHandler.LogError(err) {
+					return err
+				}
+				childInfos = filterCallback(childInfos)
+			}
+
+			return nil
+		}()
 		if err != nil {
-			if os.IsExist(err) {
-				return err
-			}
-			fInfo = newFakeFileInfo(path.Base(fsPath), true) // prefix path for alias
-		} else {
-			fInfo, err = f.Stat()
-			if h.errHandler.LogError(err) {
-				return err
-			}
+			return
 		}
-
-		if len(relPath) > 0 {
-			if err := archiveCallback(f, fInfo, relPath); err != nil {
-				return err
-			}
-		}
-
-		if f != nil && fInfo.IsDir() {
-			childInfos, err = f.Readdir(0)
-			if h.errHandler.LogError(err) {
-				return err
-			}
-			childInfos = filterCallback(childInfos)
-		}
-
-		return nil
-	}()
-	if err != nil {
-		return
+	} else {
+		fInfo = newFakeFileInfo(path.Base(fsPath), true)
 	}
 
 	if fInfo.IsDir() {
@@ -76,6 +85,7 @@ func (h *handler) visitFs(
 				continue
 			}
 
+			// just generate a path for walk down, mapped value is not important
 			if alias.isSuccessorOf(rawRequestPath) {
 				succPath := alias.urlPath[len(rawRequestPath):]
 				if succPath[0] == '/' {
@@ -89,6 +99,7 @@ func (h *handler) visitFs(
 			}
 		}
 
+		// childInfo can be regular dir/file, or aliased item that shadows regular dir/file
 		for _, childInfo := range childInfos {
 			childPath := "/" + childInfo.Name()
 			childFsPath := fsPath + childPath
@@ -96,16 +107,17 @@ func (h *handler) visitFs(
 			childRelPath := relPath + childPath
 
 			if childAliasedFsPath, hasChildAlias := childAliases[childRawRequestPath]; hasChildAlias {
-				h.visitFs(childAliasedFsPath, childRawRequestPath, childRelPath, filterCallback, archiveCallback)
-				delete(childAliases, childRawRequestPath)
+				h.visitFs(childAliasedFsPath, childRawRequestPath, childRelPath, statFs, filterCallback, archiveCallback)
+				delete(childAliases, childRawRequestPath) // delete walked alias
 			} else {
-				h.visitFs(childFsPath, childRawRequestPath, childRelPath, filterCallback, archiveCallback)
+				h.visitFs(childFsPath, childRawRequestPath, childRelPath, statFs, filterCallback, archiveCallback)
 			}
 		}
 
+		// rest aliases are standalone aliases that not shadows exists dir/file
 		for childRawRequestPath, childAliasedFsPath := range childAliases {
 			childRelPath := relPath + "/" + path.Base(childRawRequestPath)
-			h.visitFs(childAliasedFsPath, childRawRequestPath, childRelPath, filterCallback, archiveCallback)
+			h.visitFs(childAliasedFsPath, childRawRequestPath, childRelPath, statFs, filterCallback, archiveCallback)
 		}
 	}
 }
@@ -139,6 +151,7 @@ func (h *handler) archive(
 		path.Clean(h.root+pageData.handlerReqPath),
 		pageData.rawReqPath,
 		"",
+		!h.emptyRoot,
 		filterCallback,
 		func(f *os.File, fInfo os.FileInfo, relPath string) error {
 			go h.logArchive(targetFilename, relPath, r)
--- test/case/019.emptyroot.archive.bash
+++ test/case/019.emptyroot.archive.bash
@@ -6,18 +6,15 @@ cleanup() {
 
 source "$root"/lib.bash
 
-"$ghfs" -l 3003 -r "$fs"/vhost2 --archive / -a :/go:"$fs"/vhost1/go -a :/hello/world:"$fs"/vhost1/world -E '' &
+"$ghfs" -l 3003 -R --archive / -a :/go:"$fs"/vhost1/go -a :/hello/world:"$fs"/vhost1/world -E '' &
 sleep 0.05 # wait server ready
 cleanup
 
 archive="$fs"/downloaded/archive.tar.tmp
 curl_get_body 'http://127.0.0.1:3003/?tar' > "$archive"
-(tar -tf "$archive" | grep -q '^a/') || fail "a/ not in $(basename $archive)"
-(tar -tf "$archive" | grep -q '^b/') || fail "b/ not in $(basename $archive)"
-(tar -tf "$archive" | grep -q '^file1.txt') || fail "file1.txt not in $(basename $archive)"
-(tar -tf "$archive" | grep -q '^file2.txt') || fail "file2.txt not in $(basename $archive)"
 (tar -tf "$archive" | grep -q 'go/index.txt') || fail "go/index.txt not in $(basename $archive)"
 (tar -tf "$archive" | grep -q 'hello/world/index.txt') || fail "hello/world/index.txt not in $(basename $archive)"
+(tar -tf "$archive" | grep -q -E -v '^(go|hello)/') && fail "unexpected file in $(basename $archive)"
 
 cleanup
 jobs -p | xargs kill
