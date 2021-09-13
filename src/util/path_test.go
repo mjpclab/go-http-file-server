75ea0636d6e62f746365a65816759e620bd1ed47
marjune <marjune@163.com>
2021-09-14 00:01:18 +0800

    test(util/path): add test cases

--- src/util/path_test.go
+++ src/util/path_test.go
@@ -17,8 +17,44 @@ func TestHasUrlPrefixDir(t *testing.T) {
 		t.Error(full, prefix)
 	}
 
+	full = "/a/b/c"
+	prefix = "/a/b/c"
+	if !HasUrlPrefixDir(full, prefix) {
+		t.Error(full, prefix)
+	}
+
+	full = "/a/b/c"
+	prefix = "/a/e"
+	if HasUrlPrefixDir(full, prefix) {
+		t.Error(full, prefix)
+	}
+
+	full = "/a/b/c"
+	prefix = "/a/e/"
+	if HasUrlPrefixDir(full, prefix) {
+		t.Error(full, prefix)
+	}
+
 	full = "/a/b/cd"
 	prefix = "/a/b/c"
+	if HasUrlPrefixDir(full, prefix) {
+		t.Error(full, prefix)
+	}
+
+	full = "/a/b/c"
+	prefix = "/a/b/c/"
+	if HasUrlPrefixDir(full, prefix) {
+		t.Error(full, prefix)
+	}
+
+	full = "/a/b/c"
+	prefix = "/a/b/d"
+	if HasUrlPrefixDir(full, prefix) {
+		t.Error(full, prefix)
+	}
+
+	full = "/a/b/c"
+	prefix = "/a/b/de"
 	if HasUrlPrefixDir(full, prefix) {
 		t.Error(full, prefix)
 	}
