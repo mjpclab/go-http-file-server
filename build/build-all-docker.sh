b550f21ce999fd7b4b92aae5225f3a076e75dd31
marjune <marjune@163.com>
2022-04-30 17:20:19 +0800

    chore: add build target linux/riscv64

--- build/build-all-docker.sh
+++ build/build-all-docker.sh
@@ -28,7 +28,7 @@ buildByDocker() {
 }
 
 gover=latest
-builds=('linux 386' 'linux amd64' 'linux arm' 'linux arm64' 'windows 386' 'windows amd64' 'windows arm' 'windows arm64' 'darwin amd64' 'darwin arm64')
+builds=('linux 386' 'linux amd64' 'linux arm' 'linux arm64' 'linux riscv64' 'windows 386' 'windows amd64' 'windows arm' 'windows arm64' 'darwin amd64' 'darwin arm64')
 builds=("${builds[@]}" 'freebsd 386' 'freebsd amd64' 'freebsd arm' 'freebsd arm64')
 builds=("${builds[@]}" 'openbsd 386' 'openbsd amd64' 'openbsd arm' 'openbsd arm64')
 builds=("${builds[@]}" 'netbsd 386' 'netbsd amd64' 'netbsd arm' 'netbsd arm64')
