3d42210d454546156e6ef43c4d875c0c96413204
marjune <marjune@163.com>
2022-05-01 22:09:46 +0800

    refactor(util): extract `upperLowerDistance`

--- src/util/ascii.go
+++ src/util/ascii.go
@@ -1,11 +1,13 @@
 package util
 
+const upperLowerDistance byte = 'a' - 'A'
+
 func AsciiToLowerCase(input string) string {
 	buffer := []byte(input)
 
 	for i := range buffer {
 		if buffer[i] >= 'A' && buffer[i] <= 'Z' {
-			buffer[i] += 'a' - 'A'
+			buffer[i] += upperLowerDistance
 		}
 	}
 
