89b78c61fc988ce20c8f2fd11cf5da3d09a4937a
marjune <marjune@163.com>
2022-06-05 22:15:06 +0800

    feat(app): add env var to specify PID file

---
+++ test/case/037.pid.file.bash
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+cleanup() {
+	rm -f "$fs"/vhost1/pid.txt
+}
+
+source "$root"/lib.bash
+
+GHFS_PID_FILE="$fs"/vhost1/pid.txt "$ghfs" -l 3003 -r "$fs"/vhost1 &
+sleep 0.05 # wait server ready
+
+assert $(cat "$fs"/vhost1/pid.txt) $!
+
+cleanup
+jobs -p | xargs kill
