30b2605f064a88bad3079d1dc4c93a1e1536297b
marjune <marjune@163.com>
2022-06-05 17:41:23 +0800

    chore: add script to build docker image to run ghfs

--- build/build.inc.sh
+++ build/build.inc.sh
@@ -2,6 +2,6 @@ TMP='/tmp'
 OUTDIR='../output'
 MAINNAME='ghfs'
 MOD=$(go list ../src/)
-VERSION=$(git describe --abbrev=0 --tags 2> /dev/null || git rev-parse --abbrev-ref HEAD 2> /dev/null)
+source ./build.inc.version.sh
 LDFLAGS="-s -w -X $MOD/version.appVer=$VERSION"
 LICENSE='../LICENSE'
