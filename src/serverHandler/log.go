7fb20f6b4c52aa0a7b8033cae661932670f3dfe4
marjune <marjune@163.com>
2022-06-14 15:38:26 +0800

    perf(serverHandler/log): allocate memory space more accurately

--- src/serverHandler/log.go
+++ src/serverHandler/log.go
@@ -4,16 +4,20 @@ import (
 	"net/http"
 )
 
-const LOG_BUF_SIZE = 80
-
 func (h *handler) logRequest(r *http.Request) {
 	if !h.logger.CanLogAccess() {
 		return
 	}
 
-	payload := []byte(r.RemoteAddr + " " + r.Method + " " + r.RequestURI)
+	buf := make([]byte, 0, 2+len(r.RemoteAddr)+len(r.Method)+len(r.RequestURI))
 
-	h.logger.LogAccess(payload)
+	buf = append(buf, []byte(r.RemoteAddr)...) // ~ 9-47 bytes, mainly 21 bytes
+	buf = append(buf, ' ')                     // 1 byte
+	buf = append(buf, []byte(r.Method)...)     // ~ 3-4 bytes
+	buf = append(buf, ' ')                     // 1 byte
+	buf = append(buf, []byte(r.RequestURI)...)
+
+	h.logger.LogAccess(buf)
 }
 
 func (h *handler) logMutate(username, action, detail string, r *http.Request) {
@@ -21,17 +25,17 @@ func (h *handler) logMutate(username, action, detail string, r *http.Request) {
 		return
 	}
 
-	buf := make([]byte, 0, LOG_BUF_SIZE)
+	buf := make([]byte, 0, 6+len(r.RemoteAddr)+len(username)+len(action)+len(detail))
 
-	buf = append(buf, []byte(r.RemoteAddr)...) // 9-47 bytes, mainly 21 bytes
+	buf = append(buf, []byte(r.RemoteAddr)...) // ~ 9-47 bytes, mainly 21 bytes
 	if len(username) > 0 {
-		buf = append(buf, []byte(" (")...) // 2 bytes
+		buf = append(buf, ' ', '(') // 2 bytes
 		buf = append(buf, []byte(username)...)
 		buf = append(buf, ')') // 1 byte
 	}
 	buf = append(buf, ' ')               // 1 byte
-	buf = append(buf, []byte(action)...) // 5-6 bytes
-	buf = append(buf, []byte(": ")...)   // 2 bytes
+	buf = append(buf, []byte(action)...) // ~ 5-6 bytes
+	buf = append(buf, ':', ' ')          // 2 bytes
 	buf = append(buf, []byte(detail)...)
 
 	h.logger.LogAccess(buf)
@@ -42,11 +46,11 @@ func (h *handler) logUpload(username, filename, fsPath string, r *http.Request) 
 		return
 	}
 
-	buf := make([]byte, 0, LOG_BUF_SIZE)
+	buf := make([]byte, 0, 16+len(r.RemoteAddr)+len(username)+len(filename)+len(fsPath))
 
-	buf = append(buf, []byte(r.RemoteAddr)...) // 9-47 bytes, mainly 21 bytes
+	buf = append(buf, []byte(r.RemoteAddr)...) // ~ 9-47 bytes, mainly 21 bytes
 	if len(username) > 0 {
-		buf = append(buf, []byte(" (")...) // 2 bytes
+		buf = append(buf, ' ', '(') // 2 bytes
 		buf = append(buf, []byte(username)...)
 		buf = append(buf, ')') // 1 byte
 	}
@@ -63,9 +67,9 @@ func (h *handler) logArchive(filename, relPath string, r *http.Request) {
 		return
 	}
 
-	buf := make([]byte, 0, LOG_BUF_SIZE)
+	buf := make([]byte, 0, 19+len(r.RemoteAddr)+len(filename)+len(relPath))
 
-	buf = append(buf, []byte(r.RemoteAddr)...)      // 9-47 bytes, mainly 21 bytes
+	buf = append(buf, []byte(r.RemoteAddr)...)      // ~ 9-47 bytes, mainly 21 bytes
 	buf = append(buf, []byte(" archive file: ")...) // 15 bytes
 	buf = append(buf, []byte(filename)...)
 	buf = append(buf, []byte(" <- ")...) // 4 bytes
