5b79a519d23f067f2d682cdc781e1b410c6be673
marjune <marjune@163.com>
2022-08-24 00:18:34 +0800

    feat(serverHandler): remove `isVirtual` from json
    The `isVirtual` should not leak to user, although it is possible
    to detect.

--- src/serverHandler/json.go
+++ src/serverHandler/json.go
@@ -9,11 +9,10 @@ import (
 )
 
 type jsonItem struct {
-	IsDir     bool      `json:"isDir"`
-	IsVirtual bool      `json:"isVirtual"`
-	Name      string    `json:"name"`
-	Size      int64     `json:"size"`
-	ModTime   time.Time `json:"modTime"`
+	IsDir   bool      `json:"isDir"`
+	Name    string    `json:"name"`
+	Size    int64     `json:"size"`
+	ModTime time.Time `json:"modTime"`
 }
 
 type jsonResponseData struct {
@@ -37,11 +36,10 @@ type jsonResponseData struct {
 
 func getJsonItem(info os.FileInfo) *jsonItem {
 	return &jsonItem{
-		IsDir:     info.IsDir(),
-		IsVirtual: isVirtual(info),
-		Name:      info.Name(),
-		Size:      info.Size(),
-		ModTime:   info.ModTime(),
+		IsDir:   info.IsDir(),
+		Name:    info.Name(),
+		Size:    info.Size(),
+		ModTime: info.ModTime(),
 	}
 }
 
