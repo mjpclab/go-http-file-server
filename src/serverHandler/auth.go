8de961d0666d0b15dba1d36b68dec2af6cd9b4a3
marjune <marjune@163.com>
2022-09-04 17:09:15 +0800

    feat(serverHandler/auth): use fixed realm

--- src/serverHandler/auth.go
+++ src/serverHandler/auth.go
@@ -6,7 +6,7 @@ import (
 )
 
 func (h *aliasHandler) needAuth(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("WWW-Authenticate", "Basic realm=\""+r.URL.Path+"\"")
+	w.Header().Set("WWW-Authenticate", "Basic realm=\"files\"")
 }
 
 func (h *aliasHandler) verifyAuth(r *http.Request) (username string, success bool, err error) {
