0e324e9b3640b3008a031c79909c308684fdb6fd
marjune <marjune@163.com>
2023-06-10 11:18:26 +0800

    chore: update build config for WSL1-compat build

--- build/build-all-by-docker.sh
+++ build/build-all-by-docker.sh
@@ -40,18 +40,3 @@ buildByDocker() {
 
 gover=latest
 buildByDocker "$gover" "${builds[@]}"
-
-gover=alpine
-builds=('linux 386 -musl' 'linux amd64 -musl' 'linux amd64,v2 -musl' 'linux amd64,v3 -musl' 'linux arm64 -musl')
-buildByDocker "$gover" "${builds[@]}"
-
-gover=1.20
-builds=()
-builds+=('windows 386 -7-8' 'windows amd64 -7-8')
-#builds+=('windows amd64,v2 -7-8' 'windows amd64,v3 -7-8')
-#builds+=('darwin amd64 -10.13-high-sierra')
-buildByDocker "$gover" "${builds[@]}"
-
-#gover=1.16
-#builds=('darwin amd64 -10.12-sierra')
-#buildByDocker "$gover" "${builds[@]}"
