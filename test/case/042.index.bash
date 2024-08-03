1f6b334f1070845e4c4feb230bd1f24912b10fee
marjune <marjune@163.com>
2024-08-03 19:37:38 +0800

    feat(API): accept JSON request by header `Accept: application/json`
    BREAKING CHANGE
    
    Before:
    
    ```sh
    curl 'http://server/path?json'
    ```
    
    After:
    
    ```sh
    curl -H 'Accept: application/json' 'http://server/path'
    ```

--- test/case/042.index.bash
+++ test/case/042.index.bash
@@ -7,40 +7,40 @@ sleep 0.05 # wait server ready
 
 # --index
 
-cnt=$(curl_get_body 'http://127.0.0.1:3003/?json' | jq '.subItems | length')
+cnt=$(curl_get_body -H 'accept: application/json' 'http://127.0.0.1:3003/' | jq '.subItems | length')
 assert "$cnt" '0'
 
-cnt=$(curl_get_body 'http://127.0.0.1:3003/go/?json' | jq '.subItems | length')
+cnt=$(curl_get_body -H 'accept: application/json' 'http://127.0.0.1:3003/go/' | jq '.subItems | length')
 assert "$cnt" '0'
 
-cnt=$(curl_get_body 'http://127.0.0.1:3003/hello/?json' | jq '.subItems | length')
+cnt=$(curl_get_body -H 'accept: application/json' 'http://127.0.0.1:3003/hello/' | jq '.subItems | length')
 [ "$cnt" == "0" ] && fail "subItems should not be 0"
 
 # --index-dir
 
-cnt=$(curl_get_body 'http://127.0.0.1:3003/world/?json' | jq '.subItems | length')
+cnt=$(curl_get_body -H 'accept: application/json' 'http://127.0.0.1:3003/world/' | jq '.subItems | length')
 [ "$cnt" == "0" ] && fail "subItems should not be 0"
 
 # --index-user
 
-cnt=$(curl_get_body 'http://127.0.0.1:3003/x/y/z/a/?json' | jq '.subItems | length')
+cnt=$(curl_get_body -H 'accept: application/json' 'http://127.0.0.1:3003/x/y/z/a/' | jq '.subItems | length')
 assert "$cnt" '0'
 
-cnt=$(curl_get_body 'http://baz:789@127.0.0.1:3003/x/y/z/a/?json' | jq '.subItems | length')
+cnt=$(curl_get_body -H 'accept: application/json' 'http://baz:789@127.0.0.1:3003/x/y/z/a/' | jq '.subItems | length')
 assert "$cnt" '0'
 
-cnt=$(curl_get_body 'http://foo:123@127.0.0.1:3003/x/y/z/a/?json' | jq '.subItems | length')
+cnt=$(curl_get_body -H 'accept: application/json' 'http://foo:123@127.0.0.1:3003/x/y/z/a/' | jq '.subItems | length')
 [ "$cnt" == "0" ] && fail "subItems should not be 0"
 
 # --index-dir-user
 
-cnt=$(curl_get_body 'http://127.0.0.1:3003/x/y/z/b/?json' | jq '.subItems | length')
+cnt=$(curl_get_body -H 'accept: application/json' 'http://127.0.0.1:3003/x/y/z/b/' | jq '.subItems | length')
 assert "$cnt" '0'
 
-cnt=$(curl_get_body 'http://baz:789@127.0.0.1:3003/x/y/z/b/?json' | jq '.subItems | length')
+cnt=$(curl_get_body -H 'accept: application/json' 'http://baz:789@127.0.0.1:3003/x/y/z/b/' | jq '.subItems | length')
 assert "$cnt" '0'
 
-cnt=$(curl_get_body 'http://bar:456@127.0.0.1:3003/x/y/z/b/?json' | jq '.subItems | length')
+cnt=$(curl_get_body -H 'accept: application/json' 'http://bar:456@127.0.0.1:3003/x/y/z/b/' | jq '.subItems | length')
 [ "$cnt" == "0" ] && fail "subItems should not be 0"
 
 jobs -p | xargs kill &> /dev/null
--- test/lib.bash
+++ test/lib.bash
@@ -1,52 +1,52 @@
 #!/bin/bash
 
 assert() {
-	expect="$2"
-	actual="$1"
+	local expect="$2"
+	local actual="$1"
 	if [ "$expect" != "$actual" ]; then
 		echo -e "$(basename $0):${BASH_LINENO[0]} expect \"\e[0;32m$expect\e[0m\", got \"\e[1;31m$actual\e[0m\"" >&2
 	fi
 }
 
 fail() {
-	msg="$1"
+	local msg="$1"
 	echo -e "$(basename $0):${BASH_LINENO[0]} \e[1;31m$msg\e[0m" >&2
 }
 
 curl_head_status() {
-	args=($@)
-	urlindex=$[ ${#args[@]} - 1 ]
-	opts="${args[@]:0:urlindex}"
-	url="${args[urlindex]}"
+	local args=("$@")
+	local urlindex=$[ ${#args[@]} - 1 ]
+	local opts=("${args[@]:0:urlindex}")
+	local url="${args[urlindex]}"
 
-	curl -s -k -I $opts "$url" | head -n 1 | cut -d ' ' -f 2
+	curl -s -k -I "${opts[@]}" "$url" | head -n 1 | cut -d ' ' -f 2
 }
 
 curl_get_status() {
-	args=($@)
-	urlindex=$[ ${#args[@]} - 1 ]
-	opts="${args[@]:0:urlindex}"
-	url="${args[urlindex]}"
+	local args=("$@")
+	local urlindex=$[ ${#args[@]} - 1 ]
+	local opts=("${args[@]:0:urlindex}")
+	local url="${args[urlindex]}"
 
-	curl -s -k -i $opts "$url" | head -n 1 | cut -d ' ' -f 2
+	curl -s -k -i "${opts[@]}" "$url" | head -n 1 | cut -d ' ' -f 2
 }
 
 curl_get_header() {
-	args=($@)
-	urlindex=$[ ${#args[@]} - 1 ]
-	opts="${args[@]:0:urlindex}"
-	url="${args[urlindex]}"
+	local args=("$@")
+	local urlindex=$[ ${#args[@]} - 1 ]
+	local opts=("${args[@]:0:urlindex}")
+	local url="${args[urlindex]}"
 
-	curl -s -k -i $opts "$url" | sed -e '/^$/q'
+	curl -s -k -i "${opts[@]}" "$url" | sed -e '/^$/q'
 }
 
 curl_get_body() {
-	args=($@)
-	urlindex=$[ ${#args[@]} - 1 ]
-	opts="${args[@]:0:urlindex}"
-	url="${args[urlindex]}"
+	local args=("$@")
+	local urlindex=$[ ${#args[@]} - 1 ]
+	local opts=("${args[@]:0:urlindex}")
+	local url="${args[urlindex]}"
 
-	curl -s -k $opts "$url"
+	curl -s -k "${opts[@]}" "$url"
 }
 
 curl_post_status() {
@@ -54,9 +54,9 @@ curl_post_status() {
 }
 
 curl_upload_content() {
-	url="$1"
-	name="$2"
-	value="$3"
-	filename="$4"
+	local url="$1"
+	local name="$2"
+	local value="$3"
+	local filename="$4"
 	curl -s -k -F "$name=$value;filename=$filename" "$url"
 }
