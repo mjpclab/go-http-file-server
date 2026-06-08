cb3033d4ed6926d4227216a2659daaff00791d12
marjune <marjune@163.com>
2026-06-08 22:21:00 +0800

    chore(build/docker): publish image to ghcr.io

--- build/build-docker-image.sh
+++ build/build-docker-image.sh
@@ -3,17 +3,19 @@
 cd "$(dirname "$0")"
 source ./build.inc.version.sh
 
-TAG_PREFIX=${TAG_PREFIX:-mjpclab/ghfs}
+tags=(
+	-t "mjpclab/ghfs:latest"
+	-t "mjpclab/ghfs:$VERSION"
+	-t "ghcr.io/mjpclab/ghfs:latest"
+	-t "ghcr.io/mjpclab/ghfs:$VERSION"
+)
 
 docker buildx create --name ghfs-builder --driver docker-container --bootstrap
 docker buildx use ghfs-builder
 
 docker buildx build \
-	-t "$TAG_PREFIX:latest" \
-	-t "$TAG_PREFIX:$VERSION" \
+	"${tags[@]}" \
 	-f ./build-docker-image-dockerfile \
 	--platform linux/amd64,linux/386,linux/arm64,linux/arm/v7,linux/riscv64 \
 	--push \
-	--build-arg GOOS=linux \
-	--build-arg GOARCH=amd64 \
 	../
