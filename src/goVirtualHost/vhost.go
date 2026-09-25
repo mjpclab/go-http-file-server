c2d6305133e9dfc877462428a3aeaa1b4d539e05
marjune <marjune@163.com>
2026-09-25 19:32:34 +0800

    feat: upgrade goVirtualHost

--- src/goVirtualHost/vhost.go
+++ src/goVirtualHost/vhost.go
@@ -2,23 +2,21 @@ package goVirtualHost
 
 import (
 	"crypto/tls"
-	"crypto/x509"
 	"errors"
 	"net/http"
 	"strings"
 )
 
 func newVhost(hostNames []string, certKeyPaths certKeyPairs, vhCerts certs, handler http.Handler) *vhost {
-	loadedCerts := make(certs, 0, len(certKeyPaths)+len(vhCerts))
-	loadedCerts = append(loadedCerts, vhCerts...)
+	vhCerts.makeLeaf()
 
 	vhost := &vhost{
 		hostNames:    hostNames,
 		certKeyPaths: certKeyPaths,
 		certs:        vhCerts,
-		loadedCerts:  loadedCerts,
 		handler:      handler,
 	}
+	vhost.loadedCerts.Store(&vhCerts)
 
 	return vhost
 }
@@ -41,34 +39,36 @@ func (vh *vhost) matchHostName(name string) bool {
 }
 
 func (vh *vhost) loadCertificates() []error {
-	loadedCerts, errs := LoadCertificatesFromPairs(vh.certKeyPaths)
+	fileCerts, errs := LoadCertificatesFromPairs(vh.certKeyPaths)
+	loadedCerts := certs(fileCerts)
+	loadedCerts.makeLeaf()
 
-	vh.loadedCerts = vh.loadedCerts[0:0]
-	vh.loadedCerts = append(vh.loadedCerts, loadedCerts...)
-	vh.loadedCerts = append(vh.loadedCerts, vh.certs...)
+	loadedCerts = append(loadedCerts, vh.certs...)
+
+	vh.loadedCerts.Store(&loadedCerts)
 
 	return errs
 }
 
 func (vh *vhost) lookupCertificate(hello *tls.ClientHelloInfo) (*tls.Certificate, error) {
-	certLen := len(vh.loadedCerts)
-	if certLen == 1 {
-		return vh.loadedCerts[0], nil
+	certs := *vh.loadedCerts.Load()
+	if len(certs) == 0 {
+		return nil, errors.New("cannot find proper certificate for " + hello.ServerName)
+	}
+	if len(certs) == 1 {
+		return certs[0], nil
 	}
 
-	for _, cert := range vh.loadedCerts {
+	for _, cert := range certs {
 		err := hello.SupportsCertificate(cert)
 		if err == nil {
 			return cert, err
 		}
 	}
 
-	for _, cert := range vh.loadedCerts {
+	for _, cert := range certs {
 		if cert.Leaf == nil {
-			cert.Leaf, _ = x509.ParseCertificate(cert.Certificate[0])
-			if cert.Leaf == nil {
-				continue
-			}
+			continue
 		}
 		err := cert.Leaf.VerifyHostname(hello.ServerName)
 		if err == nil {
@@ -76,9 +76,5 @@ func (vh *vhost) lookupCertificate(hello *tls.ClientHelloInfo) (*tls.Certificate
 		}
 	}
 
-	if certLen > 0 {
-		return vh.loadedCerts[0], nil
-	}
-
-	return nil, errors.New("cannot find proper certificate for " + hello.ServerName)
+	return certs[0], nil
 }
